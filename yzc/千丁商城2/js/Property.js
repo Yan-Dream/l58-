cb1861226e5a4651e4bca237d3ee3e9756ab0720
cc <798973262@qq.com>
2020-06-08 08:58:32 +0800

    yzc

--- yzc/千丁商城2/js/Property.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $(".con2_cum").children().children(".con2_tp").on("click",function(){
-        var $ind=$(this).parent().parent().index();
-        $(this).parent().parent().addClass("active").siblings().removeClass("active");
-        $(this).next().next().css("display","block").parent().parent().siblings().children().children("img").css("display","none");
-        $(".con2_coling").eq($ind).css("display","block").siblings().css("display","none");
-    })
-    console.log($(".con2_tp"));
-})
--- 宜家--靳玉鑫/宜家--靳玉鑫/js/index.js
+++
@@ -1,6 +0,0 @@
-/* 返回顶部开始 */
-function topup(){
-    window.scrollTo(0,0);
-
-}
- /* 返回顶部结束 */
